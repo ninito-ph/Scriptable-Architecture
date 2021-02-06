db0a654e8a300dbbb2f678c6d774904fa51e1455
Paulo H <paulo@ninito.me>
2021-02-05 21:46:01 -0300

    gitflow-feature-stash: reorganize-folder-structure

---
+++ Assets/Scripts/CreateMenus.cs
@@ -0,0 +1,12 @@
+﻿namespace HybridTypes
+{
+    /// <summary>
+    /// The create menu file for Hybrid Types
+    /// </summary>
+    public sealed class CreateMenus
+    {
+        public const string intHybridFileName = "Int Hybrid";
+        public const string intHybridMenu = "Hybrid Types/Int Hybrid";
+        public const int intHybridOrder = 0;
+    }
+}
---
+++ Assets/Scripts/Hybrid Types/IntMeta.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HybridTypes
+{
+    [CreateAssetMenu(fileName = "Meta Int", menuName = "Meta Int", order = 0)]
+    public class IntMeta : BaseMeta<int, BaseHybrid<int>>
+    {
+        public
+    }
+}
