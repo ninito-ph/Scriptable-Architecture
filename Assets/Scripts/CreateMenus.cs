26cbabb58b6d0dd5e5c943ad9769254d43d651a1
Paulo H <paulo@ninito.me>
2021-02-06 02:38:56 -0300

    Add new classes to CreateMenus

--- Assets/Scripts/CreateMenus.cs
+++ Assets/Scripts/CreateMenus.cs
@@ -1,12 +1,36 @@
-﻿namespace HybridTypes
+﻿namespace ScriptableArchitecture
 {
     /// <summary>
-    /// The create menu file for Hybrid Types
+    /// The create menu file for ScriptableArchitecture
     /// </summary>
     public sealed class CreateMenus
     {
-        public const string intHybridFileName = "Int Hybrid";
-        public const string intHybridMenu = "Hybrid Types/Int Hybrid";
-        public const int intHybridOrder = 0;
+        public const string intReferenceFileName = "Int Reference";
+        public const string intReferenceMenu = "Variables/Int Reference";
+        public const int intReferenceOrder = 0;
+
+        public const string floatReferenceFileName = "Float Reference";
+        public const string floatReferenceMenu = "Variables/Float Reference";
+        public const int floatReferenceOrder = 1;
+
+        public const string boolReferenceFileName = "Bool Reference";
+        public const string boolReferenceMenu = "Variables/Bool Reference";
+        public const int boolReferenceOrder = 2;
+
+        public const string StringReferenceFileName = "String Reference";
+        public const string StringReferenceMenu = "Variables/String Reference";
+        public const int StringReferenceOrder = 3;
+
+        public const string Vector2ReferenceFileName = "Vector2 Reference";
+        public const string Vector2ReferenceMenu = "Variables/Vector2 Reference";
+        public const int Vector2ReferenceOrder = 4;
+
+        public const string Vector3ReferenceFileName = "Vector3 Reference";
+        public const string Vector3ReferenceMenu = "Variables/Vector3 Reference";
+        public const int Vector3ReferenceOrder = 5;
+
+        public const string ColorReferenceFileName = "Color Reference";
+        public const string ColorReferenceMenu = "Variables/Color Reference";
+        public const int ColorReferenceOrder = 6;
     }
 }
