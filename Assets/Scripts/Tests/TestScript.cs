9a2a2c9bb9229f5c43940e293672752a037370cc
Paulo H <paulo@ninito.me>
2021-03-23 08:48:05 -0300

    Add new test script

---
+++ Assets/Scripts/Tests/TestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using ScriptableArchitecture.Variables;
+
+public class TestScript : MonoBehaviour
+{
+    [Header("Int Reference Types")]
+    [SerializeField]
+    public IntReference _myIntReferenceAsConstant = new IntReference(0);
+    [SerializeField]
+    public Vector4Reference _myIntReferenceAsVariable;
+
+    public Vector3Reference _vec3;
+
+    public Vector4 _vec3normal;
+
+    [Header("Int Variable Types")]
+    [SerializeField]
+    public IntVariable _IntVariable;
+}
