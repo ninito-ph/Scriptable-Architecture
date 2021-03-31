e88cd3c301720cf4f083e0bb86570a0fe73693bc
Paulo H <paulo@ninito.me>
2021-03-31 01:21:02 -0300

    Add Vector4 and AnimationCurve references

--- Assets/Scripts/CreateMenus.cs
+++ Assets/Scripts/CreateMenus.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// The create menu file for ScriptableArchitecture
     /// </summary>
-    public sealed class CreateMenus
+    public static class CreateMenus
     {
         public const string intVariableFileName = "Int Variable";
         public const string intVariableMenu = "Variables/Int Variable";
@@ -28,13 +28,21 @@
         public const string Vector3VariableFileName = "Vector3 Variable";
         public const string Vector3VariableMenu = "Variables/Structs/Vector3 Variable";
         public const int Vector3VariableOrder = 5;
+        
+        public const string Vector4VariableFileName = "Vector4 Variable";
+        public const string Vector4VariableMenu = "Variables/Structs/Vector4 Variable";
+        public const int Vector4VariableOrder = 6;
 
         public const string ColorVariableFileName = "Color Variable";
         public const string ColorVariableMenu = "Variables/Structs/Color Variable";
-        public const int ColorVariableOrder = 6;
+        public const int ColorVariableOrder = 7;
 
         public const string GameObjectVariableFileName = "GameObject Variable";
         public const string GameObjectVariableMenu = "Variables/Classes/GameObject Variable";
-        public const int GameObjectVariableOrder = 7;
+        public const int GameObjectVariableOrder = 8;
+        
+        public const string AnimationCurveVariableFileName = "AnimationCurve Variable";
+        public const string AnimationCurveVariableMenu = "Variables/Classes/AnimationCurve Variable";
+        public const int AnimationCurveVariableOrder = 9;
     }
 }
---
+++ Assets/Scripts/Variables/Types/Reference/AnimationCurveReference.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ScriptableArchitecture.Variables
+{
+    [System.Serializable]
+    public class AnimationCurveReference : Reference<AnimationCurve, AnimationCurveVariable>
+    {
+        public AnimationCurveReference(AnimationCurve value) : base(value)
+        {
+        }
+    }
+}
